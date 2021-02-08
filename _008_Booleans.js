de41379622b2f247350f78282fb0672fc1794109
elexier <pazamoryperdon@gmail.com>
2021-02-08 11:01:50 -0500

    Booleans

---
+++ _008_Booleans.js
@@ -0,0 +1,17 @@
+/**BOOLEANS
+ * El constructor del boolean permite capturar otro tipo de datos como true o false
+ */
+
+
+let verdadero = true
+let falso = false
+
+console.log(verdadero)
+console.log(falso)
+
+console.log(typeof verdadero, typeof falso)
+console.log(Boolean(0),'cero')//vale false
+console.log(Boolean(1),'1')//true
+console.log(Boolean(-10),'-10')//true
+console.log(Boolean(""),"")//false
+console.log(Boolean(" "),' ')//true
