a0d3b4946e697c28d008382a1755dd01ae045db4
elexier <pazamoryperdon@gmail.com>
2021-02-08 11:13:31 -0500

    Undefined_Null_NaN

---
+++ _009_Undefined_Null_NaN.js
@@ -0,0 +1,16 @@
+/**UNDEFINED NULL NaN
+ * 
+ * -Undefined indica un valor ausente, normalmente cuando una variable no se ha inicializado
+ * -Null indica ausencia de un valor
+ * -NaN significa Not a Number
+ */
+
+let vacia
+console.log(vacia)
+
+let nulo = null
+console.log(nulo)
+
+let noEsNumero = "hola" * 3.7
+console.log(noEsNumero)
+
