4c093112ffd653800c048290675206530b6e6eb0
elexier <pazamoryperdon@gmail.com>
2021-02-22 19:13:55 -0500

    Objeto Math

---
+++ _028_Objeto_Math.js
@@ -0,0 +1,12 @@
+/**OBJETO MATH
+ * 
+ */
+
+console.log(Math)
+console.log(Math.ceil(3.3))
+console.log(Math.floor(3.9))
+console.log(Math.pow(2,5))//exponente 2 a la 5 da 32
+console.log(Math.sqrt(9))
+console.log(Math.sign(-38))//devuelve -1 0 1
+console.log(Math.random())//aleatorio entre 0 y 1
+console.log(Math.round(Math.random() * 1000))
