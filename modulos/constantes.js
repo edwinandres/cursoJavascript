f75cb95ad0c0b8746ff4eff84285e33e40ef71b8
elexier <pazamoryperdon@gmail.com>
2021-04-03 17:37:50 -0500

    Import and Export

---
+++ modulos/constantes.js
@@ -0,0 +1,16 @@
+export const PI = Math.PI;
+
+export let usuario = "Edwin"
+let password = "123"
+export default password
+
+export default function saludar(){
+    console.log("hola, esto es ecma6")
+}
+
+
+export default class Saludar{
+    constructor(){
+        console.log("Hola Modulos + ES6")
+    }
+}
---
+++ modulos/modulos.js
@@ -0,0 +1,8 @@
+import saludar, { PI, usuario } from "./constantes.js";
+import {aritmetica} from "./aritmetica.js";
+
+console.log("hola")
+console.log(PI)
+
+console.log(aritmetica.sumar(3,6))
+saludar()
